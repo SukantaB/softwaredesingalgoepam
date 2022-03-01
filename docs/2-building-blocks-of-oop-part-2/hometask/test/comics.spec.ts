fe47ad0d0b849f067b094b77d8b68bfaadbbbc59
Sukanta Saha <sukanta_saha@epam.com>
2022-03-01 18:55:14 +0530

    fix: revert test file changes

--- docs/2-building-blocks-of-oop-part-2/hometask/test/comics.spec.ts
+++ docs/2-building-blocks-of-oop-part-2/hometask/test/comics.spec.ts
@@ -1,10 +1,14 @@
+import {Pages} from "../src/pages";
+import {Page} from "../src/page";
 import {Comics} from "../src/comics";
-import { PagesFactory } from "../src/pagesFactory";
 
 describe('Comics', () => {
   it('toString should return correct value', () => {
     let counter = 1;
-    const comics = new Comics('Spider-Man', 'Stan Lee', 'some author', PagesFactory.build([[1, 'with images', 'glossy paper'], [2, 'with images', 'glossy paper']]) );
+    const comics = new Comics('Spider-Man', 'Stan Lee', 'some author', new Pages([
+      new Page(1, 'with images', 'glossy paper'),
+      new Page(2, 'with images', 'glossy paper')
+    ]));
 
     for (const page of comics) {
       expect(page).toEqual(`Comics: Spider-Man by Stan Lee, the artist is some author, number of pages: 2, here is page with images #${counter} and it\'s material is glossy paper`);
